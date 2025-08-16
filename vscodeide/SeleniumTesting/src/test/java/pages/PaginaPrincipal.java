84a253d4e233d957a29cb7d66327143819ed8fbf
martincorona007 <martincorona007@hotmail.com>
2025-08-16 07:37:58 -0600

    16092025 - updates

--- vscodeide/SeleniumTesting/src/test/java/pages/PaginaPrincipal.java
+++ vscodeide/SeleniumTesting/src/test/java/pages/PaginaPrincipal.java
@@ -7,7 +7,10 @@ public class PaginaPrincipal extends BasePage {
     public void navigateToFreeRangeTesters(){
         navigateTo("https://www.freerangetesters.com");
     }
-    public void navigateToYoutube(){
+    public void navigateToYoutubeEnglish(){
         navigateTo("https://www.youtube.com/watch?v=q6YQqoGLAhU");
+    }
+    public void navigateToYoutubeSWE(){
+        navigateTo("https://www.youtube.com/watch?v=_nVB2B-M3rs");
     }
 }
